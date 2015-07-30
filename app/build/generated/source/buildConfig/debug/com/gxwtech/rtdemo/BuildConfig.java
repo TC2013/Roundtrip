61ece6568e65574438d6d7d6b83d10cff37abc37
Geoff Wyche <geoff@gxwtech.com>
2015-07-30 12:33:29 -0500

    Fixed endpoint closing in USB driver, made MongoClient a singleton to prevent multiple mongodb client connections

--- app/build/generated/source/buildConfig/debug/com/gxwtech/rtdemo/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gxwtech/rtdemo/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.gxwtech.rtdemo";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 82;
-  public static final String VERSION_NAME = "alpha-24-g7efcbf2-dirty";
+  public static final int VERSION_CODE = 83;
+  public static final String VERSION_NAME = "alpha-25-g912b355-dirty";
 }
